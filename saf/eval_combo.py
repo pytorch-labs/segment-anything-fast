a6012a9923cc89fb23aff1b2ad809e6b12645933
Christian Puhrsch <cpuhrsch@fb.com>
2023-09-20 16:24:59 -0700

    Big batches

--- saf/eval_combo.py
+++ saf/eval_combo.py
@@ -13,22 +13,27 @@ def unbind_jagged(device, data, sizes, offsets):
     data = data.to(device=device, non_blocking=True)
     return [data[offsets[batch_idx]:offsets[batch_idx+1]].view(sizes[batch_idx]) for batch_idx in range(len(sizes))]
 
+PADDED_TENSOR=None
 
-def pad_to_batch_size(batch, batch_size):
-    # This avoids recompilation
-    if batch.size(0) < batch_size:
-        assert batch.dim() == 4
+# Preallocate a "landing" Tensor for incoming data and reuse it across launches.
+def pad_to_batch_size(batch, batch_size, device):
+    assert batch.dim() == 4
+    assert batch.is_pinned()
+    global PADDED_TENSOR
+    if PADDED_TENSOR is None:
+        batch = batch.to(device=device, non_blocking=True)
         full_batch_size = (batch_size, batch.size(1), batch.size(2), batch.size(3))
         first_entry = batch[0].unsqueeze(0)
         repeat_first_entry = first_entry.expand(full_batch_size)
         padded_batch = torch.cat([batch, repeat_first_entry[batch.size(0):batch_size]], dim=0)
         assert padded_batch.size() == full_batch_size
-        return padded_batch
-    return batch
+        PADDED_TENSOR = padded_batch
+    PADDED_TENSOR[:batch.size(0)].copy_(batch, non_blocking=True)
+    return PADDED_TENSOR
 
-def get_features_batch(encoder, input_image_batch, pad_input_image_batch, batch_size):
+def get_features_batch(encoder, input_image_batch, pad_input_image_batch, batch_size, device):
     if pad_input_image_batch:
-        features_batch = encoder(pad_to_batch_size(input_image_batch, batch_size))
+        features_batch = encoder(pad_to_batch_size(input_image_batch, batch_size, device))
         return features_batch[:input_image_batch.size(0)]
     return encoder(input_image_batch)
 
@@ -37,74 +42,125 @@ def build_results_batch_nested(predictor, batch, batch_size, pad_input_image_bat
     device = predictor.device
 
     input_image_batch = batch[0]
+    # The number of valid data points varies slightly per batch
+    orig_input_image_batch_size = input_image_batch.size(0)
     if input_image_batch is None:
-        return (None, None)
+        return (None, None, None)
     if batch[1] is None:
-        return (None, None)
+        return (None, None, None)
 
-    input_image_batch = input_image_batch.to(device=device, non_blocking=True)
-    with torch.autograd.profiler.record_function("nt get_features_batch"):
-        features_batch = get_features_batch(encoder, input_image_batch, pad_input_image_batch, batch_size)
-    datapoints = list(zip(*(batch[7:])))
+    with torch.autograd.profiler.record_function("nt data transfer"):
+        datapoints = list(zip(*(batch[7:])))
+        nt_coords = batch[1].to(device=device, non_blocking=True)
+        nt_gt_masks = batch[4].to(device=device, non_blocking=True)
+        nt_fg_labels = torch.nested.nested_tensor([torch.ones(
+            (coords.size(0), 1), dtype=torch.int, device=device) for coords in nt_coords.unbind()])
+        if pad_input_image_batch:
+            # Pad to a static shape to avoid recompilation
+            input_image_batch = pad_to_batch_size(input_image_batch, batch_size, device)
+        else:
+            input_image_batch = input_image_batch.to(device=device, non_blocking=True)
 
-    nt_coords = batch[1].to(device=device, non_blocking=True)
-    nt_gt_masks = batch[4].to(device=device, non_blocking=True)
-    nt_fg_labels = torch.nested.nested_tensor([torch.ones(
-        (coords.size(0), 1), dtype=torch.int, device=device) for coords in nt_coords.unbind()])
-    predictor.reset_image()
-    predictor.original_sizes = [d[1].shape[:2] for d in datapoints]
-    predictor.input_sizes = [d[2] for d in datapoints]
-    predictor.features_batch = features_batch
-    predictor.is_image_set = True
-    nt_coords = nt_coords.unsqueeze(2)
-    masks, scores, logits = predictor.predict_torch(
-        point_coords=nt_coords,
-        point_labels=nt_fg_labels,
-        multimask_output=True,
-    )
-    with torch.autograd.profiler.record_function("nt create_results_entry"):
+    # We explicitly exclude data transfers from the timing to focus
+    # only on the kernel performance.
+    # Next we synchronize and set two events to start timing.
+    torch.cuda.synchronize()
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+
+    # input_image_batch = input_image_batch.to(device=device, non_blocking=True)
+    with torch.autograd.profiler.record_function("image encoder"):
+        features_batch = encoder(input_image_batch)
+        features_batch = features_batch[:orig_input_image_batch_size]
+
+    with torch.autograd.profiler.record_function("nt predict_torch"):
+        predictor.reset_image()
+        predictor.original_sizes = [d[1].shape[:2] for d in datapoints]
+        predictor.input_sizes = [d[2] for d in datapoints]
+        predictor.features_batch = features_batch
+        predictor.is_image_set = True
+        nt_coords = nt_coords.unsqueeze(2)
+        masks, scores, logits = predictor.predict_torch(
+            point_coords=nt_coords,
+            point_labels=nt_fg_labels,
+            multimask_output=True,
+        )
         result_batch = [create_result_entry(d[0], g, m, s, d[3]) for (m, s, d, g) in zip(masks.unbind(),
                                                                                  scores.unbind(), datapoints,
                                                                                  nt_gt_masks.unbind())]
-    return sum(result_batch, []), input_image_batch.size(0)
+    # After all kernels have been launched we synchronize again and measure
+    # the amount of time spent on the GPU. This is a fairly tight measurement
+    # around the launched GPU kernels and exlcudes data movement from host
+    # to device.
+    end_event.record()
+    torch.cuda.synchronize()
+    elapsed_time = start_event.elapsed_time(end_event)
+    return sum(result_batch, []), orig_input_image_batch_size, elapsed_time
 
 def build_results_batch(predictor, batch, batch_size, pad_input_image_batch):
     encoder = predictor.model.image_encoder
     device = predictor.device
 
     input_image_batch = batch[0]
+    # The number of valid data points varies slightly per batch
+    orig_input_image_batch_size = input_image_batch.size(0)
     if input_image_batch is None:
-        return (None, None)
-    input_image_batch = input_image_batch.to(device=device, non_blocking=True)
-    coords_lists = unbind_jagged(*([device] + batch[1:4]))
-    gt_masks_lists = unbind_jagged(*([device] + batch[4:7]))
-    if coords_lists is None:
-        return (None, None)
-    with torch.autograd.profiler.record_function("image encoder"):
-        features_batch = get_features_batch(encoder, input_image_batch, pad_input_image_batch, batch_size)
-    datapoints = list(zip(*(batch[7:] + [coords_lists, gt_masks_lists])))
+        return (None, None, None)
 
-    result_batch = []
-    for batch_idx, (anns, image, input_size, idx, coords, gt_masks) in enumerate(datapoints):
-        features = features_batch.narrow(0, batch_idx, 1)
-        predictor.reset_image()
-        predictor.original_size = image.shape[:2]
-        predictor.input_size = input_size
-        predictor.features = features
-        predictor.is_image_set = True
-        coords = coords.unsqueeze(1)
-        fg_labels = torch.ones(
-            (coords.size(0), 1), dtype=torch.int, device=device)
-        # TODO: Break this up further to batch more computation.
-        masks, scores, logits = predictor.predict_torch(
-            point_coords=coords,
-            point_labels=fg_labels,
-            multimask_output=True,
-        )
-        entry = create_result_entry(anns, gt_masks, masks, scores, idx)
-        result_batch += entry
-        # break
-    return result_batch, input_image_batch.size(0)
+    with torch.autograd.profiler.record_function("data transfer"):
+        coords_lists = unbind_jagged(*([device] + batch[1:4]))
+        gt_masks_lists = unbind_jagged(*([device] + batch[4:7]))
+        if coords_lists is None:
+            return (None, None, None)
+        datapoints = list(zip(*(batch[7:] + [coords_lists, gt_masks_lists])))
+        if pad_input_image_batch:
+            # Pad to a static shape to avoid recompilation
+            input_image_batch = pad_to_batch_size(input_image_batch, batch_size, device)
+        else:
+            input_image_batch = input_image_batch.to(device=device, non_blocking=True)
+
+    # We explicitly exclude data transfers from the timing to focus
+    # only on the kernel performance.
+    # Next we synchronize and set two events to start timing.
+    torch.cuda.synchronize()
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+
+    with torch.autograd.profiler.record_function("image encoder"):
+        features_batch = encoder(input_image_batch)
+        features_batch = features_batch[:orig_input_image_batch_size]
+
+    with torch.autograd.profiler.record_function("predict_torch"):
+        result_batch = []
+        for batch_idx, (anns, image, input_size, idx, coords, gt_masks) in enumerate(datapoints):
+            features = features_batch.narrow(0, batch_idx, 1)
+            predictor.reset_image()
+            predictor.original_size = image.shape[:2]
+            predictor.input_size = input_size
+            predictor.features = features
+            predictor.is_image_set = True
+            coords = coords.unsqueeze(1)
+            fg_labels = torch.ones(
+                (coords.size(0), 1), dtype=torch.int, device=device)
+            # TODO: Break this up further to batch more computation.
+            masks, scores, logits = predictor.predict_torch(
+                point_coords=coords,
+                point_labels=fg_labels,
+                multimask_output=True,
+            )
+            entry = create_result_entry(anns, gt_masks, masks, scores, idx)
+            result_batch += entry
+
+    # After all kernels have been launched we synchronize again and measure
+    # the amount of time spent on the GPU. This is a fairly tight measurement
+    # around the launched GPU kernels and exlcudes data movement from host
+    # to device.
+    end_event.record()
+    torch.cuda.synchronize()
+    elapsed_time = start_event.elapsed_time(end_event)
+    return result_batch, orig_input_image_batch_size, elapsed_time
 
 
 def build_results(batched_data_iter,
@@ -119,40 +175,38 @@ def build_results(batched_data_iter,
     # TODO: Re-enable this for datapoints
     assert not use_compile_decoder
 
+    batch_runner = None
+    if use_nested_tensor:
+        batch_runner = build_results_batch_nested
+    else:
+        batch_runner = build_results_batch
+
     results = []
     batch_idx = 0
     num_images = 0
     num_batches = 0
-    start_event = None
-    end_event = None
+    elapsed_time = 0
     for batch in tqdm.tqdm(batched_data_iter):
         with torch.no_grad():
             if batch_idx == 0:
-                if str(use_compile) != "False":
-                    predictor.model.image_encoder = torch.compile(predictor.model.image_encoder, mode=use_compile)
-            if use_nested_tensor:
-                result_batch, num_datapoints = build_results_batch_nested(predictor, batch, batch_size, pad_input_image_batch)
-            else:
-                result_batch, num_datapoints = build_results_batch(predictor, batch, batch_size, pad_input_image_batch)
+                with torch.autograd.profiler.record_function("compilation and warmup"):
+                    if str(use_compile) != "False":
+                        predictor.model.image_encoder = torch.compile(predictor.model.image_encoder, mode=use_compile)
+                    # Run first batch a few times for warmup and exclude it from the final timings
+                    for _ in range(3):
+                        _ = batch_runner(predictor, batch, batch_size, pad_input_image_batch)
+            result_batch, num_datapoints, kernel_time = batch_runner(predictor, batch, batch_size, pad_input_image_batch)
             if result_batch is not None:
                 results += result_batch
-
-        if batch_idx == 0:
-            torch.cuda.synchronize()
-            start_event = torch.cuda.Event(enable_timing=True)
-            end_event = torch.cuda.Event(enable_timing=True)
-            start_event.record()
-        else:
-            if num_datapoints is not None:
-                num_images += num_datapoints
-                num_batches += 1
+        if num_datapoints is not None:
+            num_images += num_datapoints
+            num_batches += 1
+            elapsed_time += kernel_time
         batch_idx += 1
 
-    end_event.record()
-    torch.cuda.synchronize()
     avg_ms_per_img = None
     if num_images > 0:
-        avg_ms_per_img = start_event.elapsed_time(end_event)
+        avg_ms_per_img = elapsed_time
         avg_ms_per_img = avg_ms_per_img / num_images
 
     return results, avg_ms_per_img, num_batches, num_images
