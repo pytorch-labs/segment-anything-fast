32b585a8cb2d8485b64684492ce1274f3b8a34d5
Christian Puhrsch <cpuhrsch@fb.com>
2023-09-27 20:43:15 -0700

    First up to chart

--- summary_chart.py
+++ summary_chart.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
+import fire
 
 COLORS = list(matplotlib.colors.TABLEAU_COLORS.values())
 
-def make_sub_chart(df, ax, title, category_column, value_column, ylim_low, ylim_high, data_format, label, va):
+def make_sub_chart(df, ax, title, category_column, value_column, ylim_low, ylim_high, data_format, label, va, techniques, batch_size_idx):
     x_values = []
     y_values = []
     bar_colors = []
@@ -39,7 +40,7 @@ def make_sub_chart(df, ax, title, category_column, value_column, ylim_low, ylim_
         ax.text(x, value, data_format.format(value), ha='center', va=va)
 
 
-def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, title="", relative=False, data_format=None):
+def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, techniques, batch_size_idx, title="", relative=False, data_format=None):
     category_column = "technique"
     if not isinstance(ylim_low, tuple):
         ylim_low = (ylim_low, ylim_low)
@@ -56,32 +57,40 @@ def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, t
                 vit_b_df[value_column].iloc[0])
 
         make_sub_chart(vit_b_df, ax1, f"{title} for {sam_model_type}",
-                       category_column, value_column, ylim_low, ylim_high, data_format, label, va)
+                       category_column, value_column, ylim_low, ylim_high, data_format, label, va, techniques, batch_size_idx)
     helper("vit_b", ax1, ylim_low[0], ylim_high[0], va)
     helper("vit_h", ax2, ylim_low[1], ylim_high[1], va)
 
-matplotlib.rcParams.update({'font.size': 12})
+def run(up_to):
+    matplotlib.rcParams.update({'font.size': 12})
+    
+    csv_file = "results.csv"
+    mdf_ = pd.read_csv(csv_file)
+    mdf = mdf_.dropna(subset=["batch_size"])
+    techniques = {'fp32': 0, 'bf16': 1, 'compile': 2, 'SDPA': 3, 'Triton': 4, 'NT': 5, 'int8': 6, 'sparse': 7}
+    print("techniques: ", techniques)
+    keys = [k for (k, v) in sorted(techniques.items(), key=lambda kv: kv[1])]
+    mdf = pd.concat([mdf[mdf["technique"] == keys[i]] for i in range(up_to)])
+    print("keys: ", keys)
+    import pdb; pdb.set_trace()
+    
+    fig, axs = plt.subplots(3, 2, figsize=(20, 20))
+    
+    for batch_size_idx, (batch_size, hlim, va) in enumerate(zip([32, 1], [100, 100], ["bottom", "top"])):
+        df = mdf[mdf["batch_size"] == batch_size]
+        make_row_chart(df, "img_s(avg)", *axs[0], f"Batch size {batch_size}", (0.0, 0.0), (100.0, 25.0), va, techniques, batch_size_idx,
+                       "Images per second", data_format="{:.2f}")
+        make_row_chart(df, "memory(MiB)", *axs[1], f"Batch size {batch_size}", 0, 80000, va, techniques, batch_size_idx,
+                       title="Memory savings", data_format="{:.0f}")
+        make_row_chart(df, "mIoU", *axs[2], f"Batch size {batch_size}", 0.0, 1.0, va, techniques, batch_size_idx,
+                       title="Accuracy", data_format="{:.2f}")
+    for ax in axs:
+        ax[0].legend()
+        ax[1].legend()
+    # plt.tick_params(axis='both', which='both', length=10)
+    plt.tight_layout()
+    
+    fig.savefig(f'bar_chart_{up_to}.svg', format='svg')
 
-csv_file = "results.csv"
-mdf_ = pd.read_csv(csv_file)
-mdf = mdf_.dropna(subset=["batch_size"])
-techniques = {'fp32': 0, 'bf16': 1, 'compile': 2, 'SDPA': 3, 'Triton': 4, 'NT': 5, 'int8': 6, 'sparse': 7}
-print("techniques: ", techniques)
-
-fig, axs = plt.subplots(3, 2, figsize=(20, 20))
-
-for batch_size_idx, (batch_size, hlim, va) in enumerate(zip([32, 1], [100, 100], ["bottom", "top"])):
-    df = mdf[mdf["batch_size"] == batch_size]
-    make_row_chart(df, "img_s(avg)", *axs[0], f"Batch size {batch_size}", (0.0, 0.0), (100.0, 25.0), va,
-                   "Images per second", data_format="{:.2f}")
-    make_row_chart(df, "memory(MiB)", *axs[1], f"Batch size {batch_size}", 0, 80000, va,
-                   title="Memory savings", data_format="{:.0f}")
-    make_row_chart(df, "mIoU", *axs[2], f"Batch size {batch_size}", 0.0, 1.0, va,
-                   title="Accuracy", data_format="{:.2f}")
-for ax in axs:
-    ax[0].legend()
-    ax[1].legend()
-# plt.tick_params(axis='both', which='both', length=10)
-plt.tight_layout()
-
-fig.savefig('bar_chart.svg', format='svg')
+if __name__ == "__main__":
+    fire.Fire(run)
