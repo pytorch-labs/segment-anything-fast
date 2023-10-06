99fcf7b1cf15d267a9d7fcd42417c43f7a54f657
Christian Puhrsch <cpuhrsch@fb.com>
2023-10-05 20:08:09 -0700

    Updated chart

--- summary_chart.py
+++ summary_chart.py
@@ -23,9 +23,10 @@ def make_sub_chart(df, ax, title, category_column, value_column, ylim_low, ylim_
     ax.bar(x_values, y_values, label=label, color=bar_colors)
 
     # Customize the chart labels and title
-    ax.set_xlabel(category_column)
+    # ax.set_xlabel(category_column)
     ax.set_ylabel(value_column)
-    ax.set_title(title)
+    if title is not None:
+        ax.set_title(title)
     if ylim_low is None:
         assert ylim_high is None
     else:
@@ -72,7 +73,7 @@ def make_sub_chart(df, ax, title, category_column, value_column, ylim_low, ylim_
     ax.set_xticklabels(tlabels, rotation = 0, ha="center")
 
 
-def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, techniques, batch_size_idx, up_good, up_to, title="", relative=False, data_format=None):
+def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, techniques, batch_size_idx, up_good, up_to, title=None, relative=False, data_format=None):
     category_column = "technique"
     if not isinstance(ylim_low, tuple):
         ylim_low = (ylim_low, ylim_low)
@@ -88,7 +89,7 @@ def make_row_chart(df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, t
             vit_b_df[value_column] = vit_b_df[value_column].div(
                 vit_b_df[value_column].iloc[0])
 
-        make_sub_chart(vit_b_df, ax1, f"{title} for {sam_model_type}",
+        make_sub_chart(vit_b_df, ax1, title if title is None else f"{title} {sam_model_type}",
                        category_column, value_column, ylim_low, ylim_high, data_format, label, va, techniques, batch_size_idx, up_good, up_to)
     helper("vit_b", ax1, ylim_low[0], ylim_high[0], va, up_to)
     helper("vit_h", ax2, ylim_low[1], ylim_high[1], va, up_to)
@@ -118,9 +119,9 @@ def run(up_to):
     for batch_size_idx, (batch_size, hlim, va) in enumerate(zip([32, 1], [100, 100], ["top", "bottom"])):
         df = mdf[mdf["batch_size"] == batch_size]
         make_row_chart(df, "img/s", *axs[0], f"Batch size {batch_size}", (0.0, 0.0), (100.0, 100.0), va, techniques, batch_size_idx, True, up_to,
-                       "Images per second", data_format="{:.0f}")
+                       data_format="{:.0f}")
         make_row_chart(df, "memory(GiB)", *axs[1], f"Batch size {batch_size}", 0, 60, va, techniques, batch_size_idx, False, up_to,
-                       title="Memory savings", data_format="{:.0f}")
+                       data_format="{:.0f}")
     for ax in axs[1:]:
         ax[0].legend()
         ax[1].legend()
