cf38ab51db80079c93817f3e97e77a74698c0fc3
Christian Puhrsch <cpuhrsch@fb.com>
2023-10-05 21:44:58 -0700

    Updated chart

--- summary_chart.py
+++ summary_chart.py
@@ -67,7 +67,7 @@ def make_row_chart(df, value_column, ax, label, ylim_low, ylim_high, va, techniq
 
     tlabels = []
     for i in range(up_to):
-        tlabels.append(matplotlib.text.Text(i, 0, list(techniques.values())[i]))
+        tlabels.append(matplotlib.text.Text(i, 0, list(techniques.keys())[i]))
 
     # tlabels[2] = matplotlib.text.Text(2, 0, 'compile')
     # tlabels[4] = matplotlib.text.Text(4, 0, 'Triton')
