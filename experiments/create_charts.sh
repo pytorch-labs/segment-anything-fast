92a204b4da6bb9d4841277aab0ab8ff9dbad680e
Christian Puhrsch <cpuhrsch@fb.com>
2023-11-14 07:27:47 +0000

    Creation script and charts

---
+++ experiments/create_charts.sh
@@ -0,0 +1,8 @@
+head -n  7 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_0
+head -n 13 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_1
+head -n 19 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_2
+head -n 25 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_3
+head -n 31 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_4
+head -n 37 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_5
+head -n 43 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_6
+head -n 49 results | python summary_chart.py stdin png 1,8 img_s,memory vit_b,vit_h p4d_charts/bar_chart_7
--- experiments/summary_chart.py
+++ experiments/summary_chart.py
@@ -2,10 +2,11 @@ import pandas as pd
 import fire
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
 
-COLORS = list(matplotlib.colors.TABLEAU_COLORS.values())
+COLORS = [(0.7, 0.7, 0.7), (0., 0., 0.), (0.9, 0.9, 0.9)]
 
-def make_sub_chart(batch_size_idx, techniques, df, ax, title, category_column, value_column, ylim_low, ylim_high, data_format, label, va):
+def make_sub_chart(color, techniques, df, ax, title, category_column, value_column, ylim_low, ylim_high, data_format, label, va):
     x_values = []
     y_values = []
     bar_colors = []
@@ -14,7 +15,7 @@ def make_sub_chart(batch_size_idx, techniques, df, ax, title, category_column, v
         if key in df[category_column].tolist():
             x_values.append(key)
             y_values.append(df[value_column].tolist()[x_idx])
-            bar_colors.append(COLORS[batch_size_idx])
+            bar_colors.append(color)
             x_idx += 1
         else:
             x_values.append(key)
@@ -23,7 +24,7 @@ def make_sub_chart(batch_size_idx, techniques, df, ax, title, category_column, v
     for name in df[category_column]:
         if name in techniques:
             x_coords.append(techniques[name])
-    ax.bar(x_values, y_values, label=label, color=bar_colors)
+    ax.bar(x_values, y_values, label=label, color=color, edgecolor="black")
 
     # Customize the chart labels and title
     ax.set_xlabel(category_column)
@@ -43,53 +44,89 @@ def make_sub_chart(batch_size_idx, techniques, df, ax, title, category_column, v
         ax.text(x, value, data_format.format(value), ha='center', va=va)
 
 
-def make_row_chart(batch_size_idx, techniques, df, value_column, ax1, ax2, label, ylim_low, ylim_high, va, title="", relative=False, data_format=None):
+def make_row_chart(sam_model_type, color, techniques, df, value_column, ax, label, ylim_low, ylim_high, va, title, relative=False, data_format=None):
     category_column = "technique"
-    if not isinstance(ylim_low, tuple):
-        ylim_low = (ylim_low, ylim_low)
-    if not isinstance(ylim_high, tuple):
-        ylim_high = (ylim_high, ylim_high)
 
-    def helper(sam_model_type, ax1, ylim_low, ylim_high, va):
-        vit_b_df = df[df['sam_model_type'] == sam_model_type]
+    vit_b_df = df[df['sam_model_type'] == sam_model_type]
 
-        vit_b_df = vit_b_df.copy()
+    vit_b_df = vit_b_df.copy()
 
-        if relative:
-            vit_b_df[value_column] = vit_b_df[value_column].div(
-                vit_b_df[value_column].iloc[0])
+    if relative:
+        vit_b_df[value_column] = vit_b_df[value_column].div(
+            vit_b_df[value_column].iloc[0])
 
-        make_sub_chart(batch_size_idx, techniques, vit_b_df, ax1, f"{title} for {sam_model_type}",
-                       category_column, value_column, ylim_low, ylim_high, data_format, label, va)
-    helper("vit_b", ax1, ylim_low[0], ylim_high[0], va)
-    helper("vit_h", ax2, ylim_low[1], ylim_high[1], va)
+    make_sub_chart(color, techniques, vit_b_df, ax, f"{title} for {sam_model_type}",
+                   category_column, value_column, ylim_low, ylim_high, data_format, label, va)
+
+
+METRICS = ('img_s', 'memory', 'mIoU')
+MODELS = ('vit_h', 'vit_b')
 
 def run(csv_file,
-        fig_format):
-    matplotlib.rcParams.update({'font.size': 12})
+        fig_format,
+        batch_sizes,
+        metrics=METRICS,
+        models=MODELS,
+        fig_name='bar_chart'):
+
+    if not isinstance(batch_sizes, tuple):
+        batch_sizes = (batch_sizes,)
+    print("batch_sizes: ", batch_sizes)
+
+    if not isinstance(metrics, tuple):
+        metrics = (metrics,)
+    print("metrics: ", metrics)
+    for m in metrics:
+        assert m in METRICS
+
+    if not isinstance(models, tuple):
+        models = (models,)
+    print("models: ", models)
+    for m in models:
+        assert m in MODELS
+    matplotlib.rcParams.update({'font.size': 20})
     
+    if csv_file == 'stdin':
+        import sys
+        import io
+        csv_file = io.StringIO(sys.stdin.read())
     mdf_ = pd.read_csv(csv_file)
     mdf = mdf_.dropna(subset=["batch_size"])
     techniques = {'fp32': 0, 'bf16': 1, 'compile': 2, 'SDPA': 3, 'Triton': 4, 'NT': 5, 'int8': 6, 'sparse': 7}
     print("techniques: ", techniques)
     
-    fig, axs = plt.subplots(3, 2, figsize=(20, 20))
+    fig, axs = plt.subplots(len(metrics), len(models), figsize=(len(models) * 10, len(metrics) * 8))
+    if len(models) == 1:
+        axs = np.stack([axs])
+    if len(metrics) == 1:
+        axs = np.stack([axs])
     
-    for batch_size_idx, (batch_size, hlim, va) in enumerate(zip([32, 8], [100, 100], ["bottom", "top"])):
+    for batch_size in batch_sizes:
+        color = 'black' if (batch_size == 1) else COLORS[2]
         df = mdf[mdf["batch_size"] == batch_size]
-        make_row_chart(batch_size_idx, techniques, df, "img_s(avg)", *axs[0], f"Batch size {batch_size}", (0.0, 0.0), (100.0, 25.0), va,
-                       "Images per second", data_format="{:.2f}")
-        make_row_chart(batch_size_idx, techniques, df, "memory(MiB)", *axs[1], f"Batch size {batch_size}", 0, 40000, va,
-                       title="Memory savings", data_format="{:.0f}")
-        make_row_chart(batch_size_idx, techniques, df, "mIoU", *axs[2], f"Batch size {batch_size}", 0.0, 1.0, va,
-                       title="Accuracy", data_format="{:.2f}")
+        va = "bottom"
+        if 'img_s' in metrics:
+            if 'vit_b' in models:
+                make_row_chart('vit_b', color, techniques, df, "img_s(avg)", axs[0][0], f"Batch size {batch_size}", 0.0, 120.0, va, "Images per second", data_format="{:.2f}")
+            if 'vit_h' in models:
+                make_row_chart('vit_h', color, techniques, df, "img_s(avg)", axs[0][1], f"Batch size {batch_size}", 0.0, 40.0, va, "Images per second", data_format="{:.2f}")
+        if 'memory' in metrics:
+            if 'vit_b' in models:
+                make_row_chart('vit_b', color, techniques, df, "memory(MiB)", axs[1][0], f"Batch size {batch_size}", 0, 40000, va, "Memory savings", data_format="{:.0f}")
+            if 'vit_h' in models:
+                make_row_chart('vit_h', color, techniques, df, "memory(MiB)", axs[1][1], f"Batch size {batch_size}", 0, 40000, va, "Memory savings", data_format="{:.0f}")
+        if 'mIoU' in metrics:
+            if 'vit_b' in models:
+                make_row_chart('vit_b', color, techniques, df, "mIoU", axs[2][0], f"Batch size {batch_size}", 0.0, 1.0, va, "Accuracy", data_format="{:.2f}")
+            if 'vit_h' in models:
+                make_row_chart('vit_h', color, techniques, df, "mIoU", axs[2][1], f"Batch size {batch_size}", 0.0, 1.0, va, "Accuracy", data_format="{:.2f}")
     for ax in axs:
-        ax[0].legend()
-        ax[1].legend()
+        for a in ax:
+            a.legend()
     # plt.tick_params(axis='both', which='both', length=10)
     plt.tight_layout()
     
-    fig.savefig(f'bar_chart.{fig_format}', format=fig_format)
+    fig.savefig(f'{fig_name}.{fig_format}', format=fig_format)
 
 if __name__ == '__main__':
     fire.Fire(run)
