365c11db4e8678fa0c83b0ec37c8e8221ebc5806
Christian Puhrsch <cpuhrsch@fb.com>
2023-11-29 05:15:03 +0000

    Code format

--- segment_anything_fast/automatic_mask_generator.py
+++ segment_anything_fast/automatic_mask_generator.py
@@ -299,44 +299,54 @@ class SamAutomaticMaskGenerator:
 
         all_batch_data = MaskData()
         for masks, iou_preds in zip(nt_masks.unbind(), nt_iou_preds.unbind()):
-            # Serialize predictions and store in MaskData
-            data = MaskData(
-                masks=masks.flatten(0, 1),
-                iou_preds=iou_preds.flatten(0, 1),
-                points=torch.as_tensor(points.repeat(masks.shape[1], axis=0)),
-            )
-            del masks
+            self._process_batch_2(masks, iou_preds, im_size, crop_box, orig_size)
 
-            # Filter by predicted IoU
-            if self.pred_iou_thresh > 0.0:
-                keep_mask = data["iou_preds"] > self.pred_iou_thresh
-                data.filter(keep_mask)
+    def _process_batch_2(
+        self,
+        masks: torch.Tensor,
+        iou_preds: torch.Tensor,
+        im_size: Tuple[int, ...],
+        crop_box: List[int],
+        orig_size: Tuple[int, ...],
+    ) -> MaskData:
+        # Serialize predictions and store in MaskData
+        data = MaskData(
+            masks=masks.flatten(0, 1),
+            iou_preds=iou_preds.flatten(0, 1),
+            points=torch.as_tensor(points.repeat(masks.shape[1], axis=0)),
+        )
+        del masks
 
-            # Calculate stability score
-            data["stability_score"] = calculate_stability_score(
-                data["masks"], self.predictor.model.mask_threshold, self.stability_score_offset
-            )
-            if self.stability_score_thresh > 0.0:
-                keep_mask = data["stability_score"] >= self.stability_score_thresh
-                data.filter(keep_mask)
+        # Filter by predicted IoU
+        if self.pred_iou_thresh > 0.0:
+            keep_mask = data["iou_preds"] > self.pred_iou_thresh
+            data.filter(keep_mask)
 
-            # Threshold masks and calculate boxes
-            data["masks"] = data["masks"] > self.predictor.model.mask_threshold
-            data["boxes"] = batched_mask_to_box(data["masks"])
+        # Calculate stability score
+        data["stability_score"] = calculate_stability_score(
+            data["masks"], self.predictor.model.mask_threshold, self.stability_score_offset
+        )
+        if self.stability_score_thresh > 0.0:
+            keep_mask = data["stability_score"] >= self.stability_score_thresh
+            data.filter(keep_mask)
 
-            # Filter boxes that touch crop boundaries
-            keep_mask = ~is_box_near_crop_edge(data["boxes"], crop_box, [0, 0, orig_w, orig_h])
-            if not torch.all(keep_mask):
-                data.filter(keep_mask)
+        # Threshold masks and calculate boxes
+        data["masks"] = data["masks"] > self.predictor.model.mask_threshold
+        data["boxes"] = batched_mask_to_box(data["masks"])
 
-            # Compress to RLE
-            data["masks"] = uncrop_masks(data["masks"], crop_box, orig_h, orig_w)
-            # Doing this once at the end across all masks.
-            # data["rles"] = mask_to_rle_pytorch(data["masks"].cpu())
-            # Keeping the masks around is faster, even though it uses more memory.
-            # del data["masks"]
+        # Filter boxes that touch crop boundaries
+        keep_mask = ~is_box_near_crop_edge(data["boxes"], crop_box, [0, 0, orig_w, orig_h])
+        if not torch.all(keep_mask):
+            data.filter(keep_mask)
 
-            all_batch_data.cat(data)
+        # Compress to RLE
+        data["masks"] = uncrop_masks(data["masks"], crop_box, orig_h, orig_w)
+        # Doing this once at the end across all masks.
+        # data["rles"] = mask_to_rle_pytorch(data["masks"].cpu())
+        # Keeping the masks around is faster, even though it uses more memory.
+        # del data["masks"]
+
+        all_batch_data.cat(data)
         return all_batch_data
 
     @staticmethod
