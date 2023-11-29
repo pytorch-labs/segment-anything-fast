79db9cea8e144b9fa5a6143b29bafec26033f8f5
Christian Puhrsch <cpuhrsch@fb.com>
2023-11-29 04:40:38 +0000

    First wave of cleanup

--- segment_anything_fast/automatic_mask_generator.py
+++ segment_anything_fast/automatic_mask_generator.py
@@ -276,72 +276,68 @@ class SamAutomaticMaskGenerator:
     ) -> MaskData:
         orig_h, orig_w = orig_size
         nt_in_points = []
-        with torch.autograd.profiler.record_function("process batch 0"):
-            for points in all_points:
+        for points in all_points:
+            # Run model on this batch
+            transformed_points = self.predictor.transform.apply_coords(points, im_size)
+            in_points = torch.as_tensor(transformed_points) #, device=self.predictor.device)
+            # in_labels = torch.ones(in_points.shape[0], dtype=torch.int, device=in_points.device)
+            # nt_in_points.append(in_points[:, None, :])
+            nt_in_points.append(in_points)
 
-                # Run model on this batch
-                transformed_points = self.predictor.transform.apply_coords(points, im_size)
-                in_points = torch.as_tensor(transformed_points) #, device=self.predictor.device)
-                # in_labels = torch.ones(in_points.shape[0], dtype=torch.int, device=in_points.device)
-                # nt_in_points.append(in_points[:, None, :])
-                nt_in_points.append(in_points)
+        nt_in_points = torch.nested.nested_tensor(nt_in_points, layout=torch.jagged, device=self.predictor.device)
+        nt_in_labels = torch.ones_like(nt_in_points, dtype=torch.int).prod(dim=-1, keepdim=True)
+        nt_in_points = nt_in_points.unsqueeze(2)
 
-            nt_in_points = torch.nested.nested_tensor(nt_in_points, layout=torch.jagged, device=self.predictor.device)
-            nt_in_labels = torch.ones_like(nt_in_points, dtype=torch.int).prod(dim=-1, keepdim=True)
-            nt_in_points = nt_in_points.unsqueeze(2)
+        self.predictor.input_sizes = [self.predictor.input_size for _ in range(len(nt_in_points))]
+        self.predictor.original_sizes = [self.predictor.original_size for _ in range(len(nt_in_points))]
+        nt_masks, nt_iou_preds, _ = self.predictor.predict_torch(
+            point_coords=nt_in_points,
+            point_labels=nt_in_labels,
+            multimask_output=True,
+            # return_logits=True,
+        )
 
-        with torch.autograd.profiler.record_function("process batch 1"):
-            self.predictor.input_sizes = [self.predictor.input_size for _ in range(len(nt_in_points))]
-            self.predictor.original_sizes = [self.predictor.original_size for _ in range(len(nt_in_points))]
-            nt_masks, nt_iou_preds, _ = self.predictor.predict_torch(
-                point_coords=nt_in_points,
-                point_labels=nt_in_labels,
-                multimask_output=True,
-                # return_logits=True,
+        all_batch_data = MaskData()
+        for masks, iou_preds in zip(nt_masks.unbind(), nt_iou_preds.unbind()):
+            # Serialize predictions and store in MaskData
+            data = MaskData(
+                masks=masks.flatten(0, 1),
+                iou_preds=iou_preds.flatten(0, 1),
+                points=torch.as_tensor(points.repeat(masks.shape[1], axis=0)),
             )
+            del masks
 
-        with torch.autograd.profiler.record_function("process batch 2"):
-            all_batch_data = MaskData()
-            for masks, iou_preds in zip(nt_masks.unbind(), nt_iou_preds.unbind()):
-                # Serialize predictions and store in MaskData
-                data = MaskData(
-                    masks=masks.flatten(0, 1),
-                    iou_preds=iou_preds.flatten(0, 1),
-                    points=torch.as_tensor(points.repeat(masks.shape[1], axis=0)),
-                )
-                del masks
+            # Filter by predicted IoU
+            if self.pred_iou_thresh > 0.0:
+                keep_mask = data["iou_preds"] > self.pred_iou_thresh
+                data.filter(keep_mask)
 
-                # Filter by predicted IoU
-                if self.pred_iou_thresh > 0.0:
-                    keep_mask = data["iou_preds"] > self.pred_iou_thresh
-                    data.filter(keep_mask)
+            # Calculate stability score
+            data["stability_score"] = calculate_stability_score(
+                data["masks"], self.predictor.model.mask_threshold, self.stability_score_offset
+            )
+            if self.stability_score_thresh > 0.0:
+                keep_mask = data["stability_score"] >= self.stability_score_thresh
+                data.filter(keep_mask)
 
-                # Calculate stability score
-                data["stability_score"] = calculate_stability_score(
-                    data["masks"], self.predictor.model.mask_threshold, self.stability_score_offset
-                )
-                if self.stability_score_thresh > 0.0:
-                    keep_mask = data["stability_score"] >= self.stability_score_thresh
-                    data.filter(keep_mask)
+            # Threshold masks and calculate boxes
+            data["masks"] = data["masks"] > self.predictor.model.mask_threshold
+            data["boxes"] = batched_mask_to_box(data["masks"])
 
-                # Threshold masks and calculate boxes
-                data["masks"] = data["masks"] > self.predictor.model.mask_threshold
-                data["boxes"] = batched_mask_to_box(data["masks"])
+            # Filter boxes that touch crop boundaries
+            keep_mask = ~is_box_near_crop_edge(data["boxes"], crop_box, [0, 0, orig_w, orig_h])
+            if not torch.all(keep_mask):
+                data.filter(keep_mask)
 
-                # Filter boxes that touch crop boundaries
-                keep_mask = ~is_box_near_crop_edge(data["boxes"], crop_box, [0, 0, orig_w, orig_h])
-                if not torch.all(keep_mask):
-                    data.filter(keep_mask)
+            # Compress to RLE
+            data["masks"] = uncrop_masks(data["masks"], crop_box, orig_h, orig_w)
+            # Doing this once at the end across all masks.
+            # data["rles"] = mask_to_rle_pytorch(data["masks"].cpu())
+            # Keeping the masks around is faster, even though it uses more memory.
+            # del data["masks"]
 
-                # Compress to RLE
-                data["masks"] = uncrop_masks(data["masks"], crop_box, orig_h, orig_w)
-                # Doing this once at the end across all masks.
-                # data["rles"] = mask_to_rle_pytorch(data["masks"].cpu())
-                # Keeping the masks around is faster, even though it uses more memory.
-                # del data["masks"]
-
-                all_batch_data.cat(data)
-            return all_batch_data
+            all_batch_data.cat(data)
+        return all_batch_data
 
     @staticmethod
     def postprocess_small_regions(
